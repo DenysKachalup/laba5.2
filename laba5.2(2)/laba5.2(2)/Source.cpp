4969057c2f73d2ad4063980ea60f219525bcdb24
Denys Kachalup <denys-maksym.kachalup.itis.2020@lpnu.ua>
2020-11-09 18:12:17 +0200

    laba5.2

---
+++ laba5.2(2)/laba5.2(2)/Source.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+
+using namespace std;
+
+double A(const double x, const double n, double& a);
+
+double S(const double x, const double eps, int& n, double& s);
+
+
+int main()
+{
+	double x, xk, xp, eps, s = 0, dx;
+	int n = 0;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "exp(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+
+	x = xp;
+	while (xp <= xk)
+	{
+		S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << exp(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+
+	cout << "-------------------------------------------------" << endl;
+
+}
+double S(const double x, const double eps, int& n, double& s)
+{
+	n = 0;
+	double a = 1 / x;
+	s = a;
+
+	do
+	{
+		n++;
+		A(x, n, a);
+		s += a;
+	}
+	while (abs(a) >= eps);
+	return s;
+}
+
+
+double A(const double x, const double n, double& a)
+{
+	double R = ((2 * n) - 1) / 2 * ((x * x) * n) + (x * x);
+	a *= R;
+
+	return a;
+}
